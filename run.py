5febeb4b7abe071a4b6a27310cd8b63fdef0b364
ramacastro <ramicasmar@gmail.com>
2022-10-11 17:17:50 +0200

    Update

--- run.py
+++ run.py
@@ -6,50 +6,47 @@ import os
 
 URL = 'https://inf-fb95ed88-6696-4816-b355-b64c07c85e09-no4xvrhsfq-uc.a.run.app/detect'
 OCR_MODEL = 'large'
-OCR_CLASSES = 'license-plate'
+OCR_CLASS = 'license-plate'
 FOLDER_PATH = 'license-plates'
 
-start = time.time()
-wait_time = 1
-frame_to_detect = None
+seconds_to_wait = 2
 
 if not os.path.exists(FOLDER_PATH):
     os.makedirs(FOLDER_PATH)
 
-camera = cv2.VideoCapture(1)
+camera = cv2.VideoCapture(0)
 
 if camera.isOpened():
-    recording, frame = camera.read()
+    camera_open, frame = camera.read()
 else:
-    recording = False
+    camera_open = False
 
-while recording:
+start = time.time()
+
+while camera_open:
     recording, frame = camera.read()
     elapsed = time.time() - start
 
-    if elapsed >= wait_time:
+    if elapsed >= seconds_to_wait:
         start = time.time()
         image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
         now = utils.get_time()
+
         try:
-          detection_start = time.time()
-          detections = utils.detect(image_bytes, url=URL, ocr_model=OCR_MODEL, ocr_classes=OCR_CLASSES)
-          wait_time = (time.time() - detection_start) + 2
+          detections = utils.detect(image_bytes, url=URL, ocr_model=OCR_MODEL, ocr_classes=OCR_CLASS)
 
           if len(detections) > 0:
             detected_frame = utils.draw(frame, detections)
             license_plates = []
             
             for detection in detections:
-              if detection['class'] == OCR_CLASSES and detection['text']:
+              if detection['class'] == OCR_CLASS and detection['text']:
                 license_plates.append(detection['text'].upper())
             
             if len(license_plates) > 0:
               utils.save_frame(detected_frame, os.path.join(FOLDER_PATH, now + '.jpg'))
               utils.save_json(detections, os.path.join(FOLDER_PATH, now + '.json'))
               print(f'[{now}] [+] License plates saved:', ', '.join(license_plates))
-
-            detections = []
         except Exception as error:
           pass
 
